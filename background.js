44ca707e50ad10dad0c52189047ff6a6c9119286
anthony <anthonyzhengscholar@gmail.com>
2018-10-21 04:39:31 -0400

    ready for launch

--- background.js
+++ background.js
@@ -4,32 +4,119 @@ chrome.runtime.onInstalled.addListener(function() {
   })
 });
 
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+chrome.storage.sync.set({
+  playbackRate: 1,
+  degree: 0,
+  mirror: "Off",
+  mirrorCheck: false,
+  sliderValue: 0,
+  comments: "On",
+  commentsCheck: true,
+  recommended: "On",
+  recommendedCheck: true,
+}, function() {
+})
+});
 
-let initialSpeed = 1;
-let mirrorStatus = 1;
-let deg = 0;
+function getPlaybackRate() {
+  chrome.storage.sync.get(['playbackRate'], function(result) {
+  let newPlaybackRate = result.playbackRate;
+  speedUp(newPlaybackRate);
+  });
+}
+
+function getPlaybackRate2() {
+  chrome.storage.sync.get(['playbackRate'], function(result) {
+  let newPlaybackRate = result.playbackRate;
+  speedDown(newPlaybackRate);
+  });
+}
+
+function savePlaybackRate(savedPlaybackRate) {
+  chrome.storage.sync.set({'playbackRate': savedPlaybackRate}, function() {
+    // Notify that we saved.
+    console.log('Settings updated');
+    });
+}
+
+function speedUp(newspeed) {
+  newspeed += .1;
+  newspeed = Math.round(newspeed * 10) / 10;
+  // playSpeed.innerHTML = 'Speed: ' + initialSpeed + 'x';
+  savePlaybackRate(newspeed);
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {file: 'speedUp.js'}
+    )
+  });
+}
+
+function speedDown(newspeed) {
+  if (newspeed != 0) {
+  newspeed -= .1;
+  newspeed = Math.round(newspeed * 10) / 10;
+  // playSpeed.innerHTML = 'Speed: ' + initialSpeed + 'x';
+  savePlaybackRate(newspeed);
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {file: 'speedDown.js'}
+    )
+  })
+}
+}
+
+function getToggleMirror() {
+  chrome.storage.sync.get(['mirror'], function(result) {
+  let getMirror = result.mirror;
+  getToggleMirrorCheck(getMirror);
+  });
+}
+
+function getToggleMirrorCheck(getMirror) {
+  chrome.storage.sync.get(['mirrorCheck'], function(result) {
+  let getMirrorCheck = result.mirrorCheck;
+  toggleMirror(getMirror, getMirrorCheck);
+  });
+}
+
+function saveToggleMirror(savedToggleMirror, savedToggleMirrorCheck) {
+  chrome.storage.sync.set({'mirror': savedToggleMirror, 'mirrorCheck': savedToggleMirrorCheck}, function() {
+    // Notify that we saved.
+    console.log('Settings updated');
+    });
+}
+
+function toggleMirror(mirrorStatus, mirrorCheckStatus) {
+      if(mirrorStatus == "Off"){
+      mirrorStatus = "On"
+      mirrorCheckStatus = true
+      var c = 'document.getElementsByClassName("html5-video-player")[0].style.transform = "scaleX(-1)"';
+      }
+      else {
+        mirrorStatus = "Off"
+        mirrorCheckStatus = false
+        var c = 'document.getElementsByClassName("html5-video-player")[0].style.transform = "scaleX(1)"';
+      }
+      saveToggleMirror(mirrorStatus, mirrorCheckStatus)
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: c})
+      });
+}
+
+var deg = 0;
 
 chrome.commands.onCommand.addListener(function(command) {
   console.log("Command", command);
   if (command === 'toggle-speedUp') {
-    initialSpeed += .1;
-    initialSpeed = Math.round(initialSpeed * 10) / 10;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-        tabs[0].id,
-        {file: 'speedUp.js'}
-      )
-    });
+    getPlaybackRate();
   }
   if (command === 'toggle-speedDown') {
-    initialSpeed -= .1;
-    initialSpeed = Math.round(initialSpeed * 10) / 10;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-        tabs[0].id,
-        {file: 'speedDown.js'}
-      )
-    });
+    getPlaybackRate2();
   }
   if (command === 'toggle-rotate90') {
     deg -= 90
@@ -43,13 +130,6 @@ chrome.commands.onCommand.addListener(function(command) {
     });
   }
   if (command === 'toggle-mirror') {
-    mirrorStatus*=-1;
-    var c = 'document.getElementsByClassName("html5-video-player")[0].style.transform = "scalex('+mirrorStatus.toString()+')"';
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: c}
-      )
-    });
+    getToggleMirror();
   }
 });
--- buttons.js
+++ buttons.js
@@ -1,15 +1,54 @@
 'use strict';
 
-let initialSpeed = 1;
-let initDegree = 0;
-let mirrorStatus = 1;
-let commentStatus = -1;
-let recommendedStatus = -1;
 
-function speedUp() {
-  initialSpeed += .1;
-  initialSpeed = Math.round(initialSpeed * 10) / 10;
-  playSpeed.innerHTML = 'Speed: ' + initialSpeed + 'x';
+chrome.storage.sync.get(['playbackRate'], function(result) {
+  let currentSpeed = result.playbackRate;
+  playSpeed.innerHTML = 'Speed: ' + currentSpeed + 'x';
+});
+
+chrome.storage.sync.get(['mirror'], function(result) {
+  let currentMirror = result.mirror;
+  toggleMirrorText.innerHTML = 'Mirror: ' + currentMirror;
+});
+
+chrome.storage.sync.get(['mirrorCheck'], function(result) {
+  let currentMirrorCheck = result.mirrorCheck;
+  document.getElementById("toggleMirror").checked = currentMirrorCheck;
+});
+
+chrome.storage.sync.get(['sliderValue'], function(result) {
+  let slideValue = result.sliderValue;
+  myRange.value = slideValue;
+  slideRotation.innerHTML = "Rotation: " + slideValue.toString() + '°';
+});
+
+chrome.storage.sync.get(['comments'], function(result) {
+  let currentComments = result.comments;
+  toggleCommentsText.innerHTML = 'Comments: ' + currentComments;
+});
+
+chrome.storage.sync.get(['commentsCheck'], function(result) {
+  let currentCommentsCheck = result.commentsCheck;
+  document.getElementById("toggleComments").checked = currentCommentsCheck;
+});
+
+chrome.storage.sync.get(['recommended'], function(result) {
+  let currentRecommended = result.recommended;
+  toggleRecommendedText.innerHTML = 'Recommended: ' + currentRecommended;
+});
+
+chrome.storage.sync.get(['recommendedCheck'], function(result) {
+  let currentRecommendedCheck = result.recommendedCheck;
+  document.getElementById("toggleRecommended").checked = currentRecommendedCheck;
+});
+
+
+function speedUp(newspeed) {
+  newspeed += .1;
+  newspeed = Math.round(newspeed * 10) / 10;
+  // playSpeed.innerHTML = 'Speed: ' + initialSpeed + 'x';
+  playSpeed.innerHTML = 'Speed: ' + newspeed + 'x';
+  savePlaybackRate(newspeed);
   console.log(playSpeed);
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
@@ -19,17 +58,69 @@ function speedUp() {
   });
 }
 
-function speedDown() {
-  initialSpeed -= .1;
-  initialSpeed = Math.round(initialSpeed * 10) / 10;
-  playSpeed.innerHTML = 'Speed: ' + initialSpeed + 'x';
+function savePlaybackRate(savedPlaybackRate) {
+  chrome.storage.sync.set({'playbackRate': savedPlaybackRate}, function() {
+    // Notify that we saved.
+    console.log('Settings updated');
+    });
+}
+
+function getPlaybackRate() {
+  chrome.storage.sync.get(['playbackRate'], function(result) {
+  let newPlaybackRate = result.playbackRate;
+  speedUp(newPlaybackRate);
+  });
+}
+
+function getPlaybackRate2() {
+  chrome.storage.sync.get(['playbackRate'], function(result) {
+  let newPlaybackRate = result.playbackRate;
+  speedDown(newPlaybackRate);
+  });
+}
+
+function getToggleMirror() {
+  chrome.storage.sync.get(['mirror'], function(result) {
+  let getMirror = result.mirror;
+  getToggleMirrorCheck(getMirror);
+  });
+}
+
+function getToggleMirrorCheck(getMirror) {
+  chrome.storage.sync.get(['mirrorCheck'], function(result) {
+  let getMirrorCheck = result.mirrorCheck;
+  toggleMirror(getMirror, getMirrorCheck);
+  });
+}
+
+
+function speedDown(newspeed) {
+  if (newspeed != 0) {
+  newspeed -= .1;
+  newspeed = Math.round(newspeed * 10) / 10;
+  // playSpeed.innerHTML = 'Speed: ' + initialSpeed + 'x';
+  playSpeed.innerHTML = 'Speed: ' + newspeed + 'x';
+  savePlaybackRate(newspeed);
+  console.log(playSpeed);
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
       tabs[0].id,
       {file: 'speedDown.js'}
     )
-  });
+  })
 }
+}
+
+
+
+function saveSliderValue() {
+  var deg = this.value;
+  chrome.storage.sync.set({'sliderValue': deg}, function() {
+    // Notify that we saved.
+    console.log('Settings updated');
+    });
+}
+
 
 function rotate() {
     var deg = this.value;
@@ -40,69 +131,135 @@ function rotate() {
       chrome.tabs.executeScript(
         tabs[0].id,
         {code: c})
-
     });
-
 }
 
-function toggleMirror() {
-      mirrorStatus*=-1
+function saveToggleMirror(savedToggleMirror, savedToggleMirrorCheck) {
+  chrome.storage.sync.set({'mirror': savedToggleMirror, 'mirrorCheck': savedToggleMirrorCheck}, function() {
+    // Notify that we saved.
+    console.log('Settings updated');
+    });
+}
 
-      if(mirrorStatus == 1){
-        toggleMirrorText.innerHTML = "Mirror: Off";
+function toggleMirror(mirrorStatus, mirrorCheckStatus) {
+      if(mirrorStatus == "Off"){
+      mirrorStatus = "On"
+      mirrorCheckStatus = true
+      toggleMirrorText.innerHTML = "Mirror: " + mirrorStatus;
+      document.getElementById("toggleMirror").checked = mirrorCheckStatus;
+      var c = 'document.getElementsByClassName("html5-video-player")[0].style.transform = "scaleX(-1)"';
       }
       else {
-        toggleMirrorText.innerHTML = "Mirror: On";
+        mirrorStatus = "Off"
+        mirrorCheckStatus = false
+        toggleMirrorText.innerHTML = "Mirror: " + mirrorStatus;
+        document.getElementById("toggleMirror").checked = mirrorCheckStatus;
+        var c = 'document.getElementsByClassName("html5-video-player")[0].style.transform = "scaleX(1)"';
       }
-      var c = 'document.getElementsByClassName("html5-video-player")[0].style.transform = "scalex('+mirrorStatus.toString()+')"';
+      saveToggleMirror(mirrorStatus, mirrorCheckStatus)
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.executeScript(
           tabs[0].id,
           {code: c})
-
       });
 }
 
-function toggleComments() {
-    commentStatus*=-1
-    var comments = document.getElementById('sections');
-    if(commentStatus == 1){
-      toggleCommentsText.innerHTML = "Comments: Off";
-      var c = 'comments.style.visibility = "hidden"';
-    }
-    else {
-      toggleCommentsText.innerHTML = "Comments: On";
-      var c = 'comments.style.visibility = "visible"';
-    }
 
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: c})
+
+
+function getToggleComments() {
+  chrome.storage.sync.get(['comments'], function(result) {
+  let getComments = result.comments;
+  getToggleCommentsCheck(getComments);
+  });
+}
+
+function getToggleCommentsCheck(getComments) {
+  chrome.storage.sync.get(['commentsCheck'], function(result) {
+  let getCommentsCheck = result.commentsCheck;
+  toggleComments(getComments, getCommentsCheck);
+  });
+}
+
+function saveToggleComments(savedToggleComments, savedToggleCommentsCheck) {
+  chrome.storage.sync.set({'comments': savedToggleComments, 'commentsCheck': savedToggleCommentsCheck}, function() {
+    // Notify that we saved.
+    console.log('Settings updated');
     });
 }
 
-function toggleRecommended() {
-    recommendedStatus*=-1
+function toggleComments(commentStatus, commentCheckStatus) {
+  if(commentStatus == "Off"){
+  commentStatus = "On"
+  commentCheckStatus = true
+  toggleCommentsText.innerHTML = "Comments: " + commentStatus;
+  document.getElementById("toggleComments").checked = commentCheckStatus;
+  var c = 'document.getElementById("sections").style.visibility = "visible"';
+  }
+  else {
+    commentStatus = "Off"
+    commentCheckStatus = false
+    toggleCommentsText.innerHTML = "Comments: " + commentStatus;
+    document.getElementById("toggleComments").checked = commentCheckStatus;
+    var c = 'document.getElementById("sections").style.visibility = "hidden"';
+  }
+  saveToggleComments(commentStatus, commentCheckStatus)
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {code: c})
+  });
+}
 
-    if(recommendedStatus == 1){
-      toggleRecommendedText.innerHTML = "Recommended: Off";
-      var r = 'document.querySelector("div#items.style-scope.ytd-watch-next-secondary-results-renderer").style.visibility = "hidden"';
-    }
-    else {
-      toggleRecommendedText.innerHTML = "Recommended: On";
-      var r = 'document.querySelector("div#items.style-scope.ytd-watch-next-secondary-results-renderer").style.visibility = "visible"';
-    }
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: r})
+
+function getToggleRecommended() {
+  chrome.storage.sync.get(['recommended'], function(result) {
+  let getRecommended = result.recommended;
+  getToggleRecommendedCheck(getRecommended);
+  });
+}
+
+function getToggleRecommendedCheck(getRecommended) {
+  chrome.storage.sync.get(['recommendedCheck'], function(result) {
+  let getRecommendedCheck = result.recommendedCheck;
+  toggleRecommended(getRecommended, getRecommendedCheck);
+  });
+}
+
+function saveToggleRecommended(savedToggleRecommended, savedToggleRecommendedCheck) {
+  chrome.storage.sync.set({'recommended': savedToggleRecommended, 'recommendedCheck': savedToggleRecommendedCheck}, function() {
+    // Notify that we saved.
+    console.log('Settings updated');
     });
 }
 
-document.getElementById('up').addEventListener('click', speedUp);
-document.getElementById('down').addEventListener('click', speedDown);
+function toggleRecommended(recommendedStatus, recommendedCheckStatus) {
+  if(recommendedStatus == "Off"){
+  recommendedStatus = "On"
+  recommendedCheckStatus = true
+  toggleRecommendedText.innerHTML = "Recommended: " + recommendedStatus;
+  document.getElementById("toggleRecommended").checked = recommendedCheckStatus;
+  var c = 'document.querySelector("div#items.style-scope.ytd-watch-next-secondary-results-renderer").style.visibility = "visible"';
+  }
+  else {
+    recommendedStatus = "Off"
+    recommendedCheckStatus = false
+    toggleRecommendedText.innerHTML = "Recommended: " + recommendedStatus;
+    document.getElementById("toggleRecommended").checked = recommendedCheckStatus;
+    var c = 'document.querySelector("div#items.style-scope.ytd-watch-next-secondary-results-renderer").style.visibility = "hidden"';
+  }
+  saveToggleRecommended(recommendedStatus, recommendedCheckStatus)
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {code: c})
+  });
+}
+
+document.getElementById('up').addEventListener('click', getPlaybackRate);
+document.getElementById('down').addEventListener('click', getPlaybackRate2);
 document.getElementById('myRange').addEventListener('input', rotate);
-document.getElementById('toggleMirror').addEventListener('click', toggleMirror);
-document.getElementById('toggleComments').addEventListener('click', toggleComments);
-document.getElementById('toggleRecommended').addEventListener('click', toggleRecommended);
+document.getElementById('myRange').addEventListener('change', saveSliderValue);
+document.getElementById('toggleMirror').addEventListener('click', getToggleMirror);
+document.getElementById('toggleComments').addEventListener('click', getToggleComments);
+document.getElementById('toggleRecommended').addEventListener('click', getToggleRecommended);
